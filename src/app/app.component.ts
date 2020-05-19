98db29c5699722931d3ad0ddab41ceb2de2ebf83
tostos <saittosun2@gmail.com>
2020-05-19 19:33:49 +0200

    using subjects for error handling burada anlamadigim ngoninit de iki error var

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
   error = null;
+  private errorSub: Subscription;
 
-  constructor(private http: HttpClient,
-              private postsService: PostsService) {}
+  constructor(private postsService: PostsService) {}
 
   ngOnInit() {
+    this.errorSub = this.postsService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
     this.isFetching = true;
     this.postsService.onFetchPosts().subscribe(
       posts => {
@@ -27,6 +29,10 @@ export class AppComponent implements OnInit {
       error => {
       this.error = error.error.error;
     });
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 
   onCreatePost(postData: Post) {
@@ -39,10 +45,6 @@ export class AppComponent implements OnInit {
       posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
-    },
-      error => {
-      console.log(error);
-      this.error = error.error.error;
     });
   }
 
@@ -51,7 +53,7 @@ export class AppComponent implements OnInit {
     // why do I want to subscribe here? Well if I deleted all posts, I also want to clear my loaded posts array here in the component. So I will add a method here where I don't really care about the result of our request or of the response but I know that this function here will only run if it succeeded and therefore here, I will then simply just set this loaded post equal to an empty array again to reset it and with these changes to the service and to the component,
     this.postsService.onDeletePosts().subscribe(() => {
       this.loadedPosts = [];
-    })
+    });
   }
 
 
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -3,11 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  error = new Subject<string>();
 
   constructor(private http: HttpClient) { }
 
@@ -18,9 +20,13 @@ export class PostsService {
       'https://angular-maxi-http.firebaseio.com/posts.json',
       postData
     )
-    .subscribe(responseData => {
-      console.log(responseData);
-    });
+    .subscribe(
+      responseData => {
+        console.log(responseData);
+      },
+      error => {
+        this.error.next(error.message);
+      });
   }
 
   onFetchPosts() {
