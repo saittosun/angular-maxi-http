6575a04af54e8b1fd02cde50b0b8dd6b303dac3a
tostos <saittosun2@gmail.com>
2020-05-19 11:06:54 +0200

    using types with the httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post(
+      .post<{name: string}>(
         'https://angular-maxi-http.firebaseio.com/posts.json',
         postData
       )
@@ -42,9 +42,11 @@ export class AppComponent implements OnInit {
   // pipe as you learned is a method that allows you to funnel your observable data through multiple operators before they reach the subscribe method. The map operator allows us to get some data and return new data which is then automatically re-wrapped into an observable so that we can still subscribe to it, if it would not be wrapped into an observable again, we could not subscribe.
   private fetchPosts() {
     this.http
-      .get('https://angular-maxi-http.firebaseio.com/posts.json')
       // tslint:disable-next-line:max-line-length
-      .pipe(map((responseData: {[key: string]: Post}) => {// that should now return the converted response data and here the idea is that we return an array of posts instead of an object with that cryptic key which then holds our actual post. Now to convert a Javascript object which we have here to an array, we have to manually loop through all the keys and create a new array.
+      // between the angled brackets, you store the type which this response will actually return as a body once it's done. So it's the response body type which is stored here and that will then automatically be handled by the Angular HttpClient and TypeScript understands this and now knows that the response data will have this type format as you can tell and this is also available on post requests, it's available on all requests, you can use these angled brackets to add this extra piece of information which is totally optional but recommended and helpful about the data you're getting back. yukarida post request te yaptik. name dememizin sebebi tekrar request yaptigimizda console da gorduk 'name' property
+      .get<{[key: string]: Post}>('https://angular-maxi-http.firebaseio.com/posts.json')
+      // tslint:disable-next-line:max-line-length
+      .pipe(map(responseData => {// that should now return the converted response data and here the idea is that we return an array of posts instead of an object with that cryptic key which then holds our actual post. Now to convert a Javascript object which we have here to an array, we have to manually loop through all the keys and create a new array.
         const postsArray: Post[] = [];
         for (const key in responseData) {
           // tslint:disable-next-line:max-line-length
